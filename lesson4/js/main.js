ffbdee7822de25db6d91f1ff7dd05994b20f2038
Samuel Palacios <spalaciosgt@gmail.com>
2021-02-02 00:56:24 -0600

    Final

---
+++ lesson4/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    document.getElementById('copyrightYear').innerText = year;
+    document.getElementById('lastUpdated').innerText = 
+    "Last Updated " + currentDate.toLocaleDateString() + " " + 
+    currentDate.toLocaleTimeString(); 
+}
