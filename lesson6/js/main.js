36dd35224ddf6aa855697dc2a8a3e5e51479184b
Samuel Palacios <spalaciosgt@gmail.com>
2021-02-21 00:24:42 -0600

    Final

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -23,12 +23,39 @@ let currentDay = currentDate.getDay();
 if (currentDay != 5) {
     document.getElementById("messageJS").classList.toggle("hide");
 } else {
-    document.getElementById("messageJS").classList.toggle("show");    }
+    document.getElementById("messageJS").classList.toggle("show");    
+}
 
-
- // Web Font Load
- WebFont.load({
+// Web Font Load
+WebFont.load({
     google: {
         families: ["Montserrat"]
     }
 });
+
+let imagesLoad = document.querySelectorAll("img[data-src]");
+
+let loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  let observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
