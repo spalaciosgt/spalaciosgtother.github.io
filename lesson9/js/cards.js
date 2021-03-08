9a02e075e528831cc44c72d3a5337242dd4b72b8
Samuel Palacios <spalaciosgt@gmail.com>
2021-03-08 16:27:09 -0600

    Final

--- lesson9/js/cards.js
+++ lesson9/js/cards.js
@@ -12,6 +12,8 @@ fetch(requestURL)
         // Loading JSON object into towns object array
         const towns = jsonObject['towns'];
 
+        let j = 0;
+
         // Loop for rendering each prophet card
         for (let i = 0; i < towns.length; i++) {
             let cityName = towns[i].name;
@@ -20,6 +22,8 @@ fetch(requestURL)
                 cityName.localeCompare("Preston") == 0 ||
                 cityName.localeCompare("Soda Springs") == 0) {
                 
+                j++;
+
                 /*
                  Adding tags
                 */
@@ -45,13 +49,25 @@ fetch(requestURL)
                 picture.setAttribute('src', "images/" + towns[i].photo);
                 picture.setAttribute('loading', 'lazy');
                 picture.setAttribute('alt', towns[i].name);
+
+                if (j % 2 == 0) {
+                    picture.setAttribute('class', "cardImageR");  
+                } else {
+                    picture.setAttribute('class', "cardImageL");         
+                }                
                 
                 // Information
                 h2.textContent = towns[i].name;
                 h3.textContent = towns[i].motto;
                 p1.textContent = 'Year Founded: ' + towns[i].yearFounded;
                 p2.textContent = 'Population: ' + towns[i].currentPopulation;
-                p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;                
+                p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;       
+                
+                h2.setAttribute('class', "cardHeader");  
+                h3.setAttribute('class', "cardHeader");  
+                p1.setAttribute('class', "cardParagraph");  
+                p2.setAttribute('class', "cardParagraph");  
+                p3.setAttribute('class', "cardParagraph");                  
                 
                 /*
                  Final appendings
