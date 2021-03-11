6a4a25cd15a07b3d9b8d771dc4cd676fd5f52332
Samuel Palacios <spalaciosgt@gmail.com>
2021-03-11 12:44:13 -0600

    Final

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -33,6 +33,33 @@ WebFont.load({
     }
 });
 
+let imagesLoad = document.querySelectorAll("img[data-src]");
+
+let loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  let observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
